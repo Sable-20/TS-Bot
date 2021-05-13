b435ad354c2b4da1276a063fea002449e1cc13a9
sable <sablea2020@gmail.com>
2021-05-13 19:20:13 -0400

    ban and kick

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -1,25 +1,25 @@
 module.exports = {
-    name: 'kick',
-    description: 'Kick a menber',
+    name: 'ban',
+    description: 'Ban a member',
     execute(msg: any, args: any) {
-        if (msg.member.permissions.has("KICK_MEMBERS")) {
+        if (msg.member.permissions.has("BAN_MEMBERS")) {
             const user = msg.mentions.users.first();
 
             if (user) {
                 const member = msg.guild.members.cache.get(user.id);
                 if (!args[1]) {
-                    member.kick(`${user.id} Kicked by ${msg.author}`).catch((err: any) => {
+                    member.ban(`${user.id} Banned by ${msg.author}`).catch((err: any) => {
                         console.error(err);
                     });
                 }
-                member.kick(`${user.id} Kicked for ${args[1]} by ${msg.author}`).catch((err: any) => {
+                member.ban(`${user.id} Banned for ${args[1]} by ${msg.author}`).catch((err: any) => {
                     console.error(err);
                 });
             } else {
                 msg.channel.send("You need to mention a user to kick");
             }
         } else {
-            msg.channel.send("You are missing the **KICK_MEMBERS** permission");
+            msg.channel.send("You are missing the **BAN_MEMBERS** permission");
         }
     }
 }
